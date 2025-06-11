d9b01050635cda2536d727d2e66734bb1e8b280a
ningmeng618 <guboyuan@gmail.com>
2025-06-11 21:46:15 +0800

    add

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';  // <-- import HashRouter
 import App from './App.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <HashRouter>  {/* wrap App with HashRouter */}
+      <App />
+    </HashRouter>
   </StrictMode>
 );
